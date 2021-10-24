3e2b6605ebc419a1699942ad09885bee796e40c1
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-10-24 21:34:00 +0300

    created a form class.

--- awward/settings.py
+++ awward/settings.py
@@ -127,8 +127,10 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- user/forms.py
+++ user/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import *
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
@@ -8,3 +9,15 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+        
+class ProfileUpdateForms(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image', 'bio']
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        exclude = ['author', 'profile', 'likes', 'comments']
+        
+        
