1a235f6efd5278387884193aa7adcd81beeade06
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-10-24 20:45:22 +0300

    Configured the urls.py to register MEDIA_ROOT.

--- user/views.py
+++ user/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def project(request):
+    current_user = request.user
+    context = {
+        'user': current_user,
+    }
+    return render(request, 'project.html', context)
