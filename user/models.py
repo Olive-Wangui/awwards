10cb0235a5673641b58393ce8dd2d4fcc47ca42f
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-10-24 17:37:53 +0300

    created methods in the profile model.

--- user/models.py
+++ user/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
 class Profile(models.Model):
@@ -10,6 +13,28 @@ class Profile(models.Model):
     
     def __str__(self):
         return f'{self.user.username} Profile'
+    def save_profile(self):
+        super().save()
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+        
+    @classmethod
+    def get_profile(cls):
+        profile = Profile.objects.all()
+        return profile
+        
+    @receiver(post_save, sender=User)
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+            
+    @receiver(post_save, sender=User)
+    def save_profile(sender, instance, **kwargs):
+        instance.profile.save()
+        
     
 class tag(models.Model):
     name = models.CharField(max_length=50)
