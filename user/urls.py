22a929b4994d36363e5f27166bb2f37c0b42d803
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-10-26 13:32:24 +0300

    deployment to heroku.

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -10,6 +11,7 @@ urlpatterns = [
     path('new/profile/', views.new_profile, name='profile'),
     path('search/', views.search_results, name='search_results'),
     path('rating/<int:pk>/', views.rating, name='rating'),
+    path('accounts/profile/', LoginView.as_view(template_name='home.html'), name='profile'),
 ]
 
 if settings.DEBUG:
