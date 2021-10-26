ceb541fa370bfc06efb28ebb09ed15a29bd36d3c
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-10-26 08:02:54 +0300

    imported the status module that handles the status code responses.

--- user/views.py
+++ user/views.py
@@ -9,6 +9,7 @@ from django.views.decorators.csrf import csrf_protect
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializer import *
+from rest_framework import status
 
 # Create your views here.
 def home(request):
@@ -131,10 +132,17 @@ def rating(request, pk):
     return render(request, 'rating.html', {'project': project, 'form': form})
 
 class ProfileList(APIView):
-    def get(self, request, format=None)
-    all_profiles = Profile.objects.all()
-    serializers = ProfileSerializer(all_profiles, many=True)
-    return Response(serializers.data)
+    def get(self, request, format=None):
+        all_profiles = Profile.objects.all()
+        serializers = ProfileSerializer(all_profiles, many=True)
+        return Response(serializers.data)
+    
+    def post(self, request, format=None):
+        serializers = ProfileSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.data.errors, status=status.HTTP_400_BAD_REQUEST) 
 
 
     
