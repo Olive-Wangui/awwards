3a87f04d06fd357bbb9577d55c4c73b83f58fa0f
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-10-26 12:32:42 +0300

    deployment to heroku.

--- awward/settings.py
+++ awward/settings.py
@@ -19,7 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-mode = config("MODE", default="dev")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-$+!ohzhe4kqvi-1v3d_ln=(dbk)#-0b3ru@mcmsv7g8r%=y8i4'
