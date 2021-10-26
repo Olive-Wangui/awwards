5625bcd9f569f1514c6542a25d349b366a0dc007
Olive-Wangui <olive.ambuga@student.moringaschool.com>
2021-10-26 13:37:22 +0300

    deployment to heroku.

--- user/urls.py
+++ user/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('new/profile/', views.new_profile, name='profile'),
     path('search/', views.search_results, name='search_results'),
     path('rating/<int:pk>/', views.rating, name='rating'),
-    path('accounts/profile/', LoginView.as_view(template_name='home.html'), name='profile'),
+    path('accounts/profile/', LoginView.as_view(template_name='profile/profile.html'), name='profile'),
 ]
 
 if settings.DEBUG:
